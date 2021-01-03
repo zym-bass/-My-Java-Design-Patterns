fc5e11bd8c13453c6c8bf58d65edf428f9fc756f
zym-bass <824122161@qq.com>
2021-01-03 11:17:24 +0800

    ServiceLocatorPattern--初始化环境（注释）

--- Service-Locator-pattern/src/main/java/com/yiming/InitalContext/InitialContext01.java
+++ Service-Locator-pattern/src/main/java/com/yiming/InitalContext/InitialContext01.java
@@ -3,7 +3,9 @@ package com.yiming.InitalContext;
 import com.yiming.service.impl.ServiceImpl1;
 import com.yiming.service.impl.ServiceImpl2;
 
+//当缓存没有实体类对象时，执行初始化，加载接口实现类
 public class InitialContext01 {
+    
     public Object lookup(String jndiName){
         if(jndiName.equalsIgnoreCase("ServiceImpl1")){
             System.out.println("Looking up and creating a new ServiceImpl1 Object");
