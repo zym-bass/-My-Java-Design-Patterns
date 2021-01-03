a42a8c3c206bc0c6cede9f24cfcc1bff12a4f4b2
zym-bass <824122161@qq.com>
2021-01-03 11:23:30 +0800

    ServiceLocatorPattern--服务定位器（注释）

--- Service-Locator-pattern/src/main/java/com/yiming/serviceLoactor/ServiceLocator01.java
+++ Service-Locator-pattern/src/main/java/com/yiming/serviceLoactor/ServiceLocator01.java
@@ -4,12 +4,18 @@ import com.yiming.InitalContext.InitialContext01;
 import com.yiming.service.Service;
 import com.yiming.cache.Cache01;
 
+//服务定位器
 public class ServiceLocator01 {
+    //创建对应的缓存对象
     private static Cache01 cache;
+
+    //在类加载的时候 创建对象
     static {
         cache = new Cache01();
     }
 
+    //获取Service的实体类，该方法使用了缓存对象和初始化环境对象，
+    //当缓存没有时，调用初始化环境，进行初始化，并添加进缓存对象中的容器中
     public static Service getService(String jndiName){
 
         Service service = cache.getService(jndiName);
