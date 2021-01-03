b3a161ec31d6705cf64ed6acc4aac3c5f6efe366
zym-bass <824122161@qq.com>
2021-01-03 11:23:20 +0800

    Update InitialContext01.java

--- Service-Locator-pattern/src/main/java/com/yiming/InitalContext/InitialContext01.java
+++ Service-Locator-pattern/src/main/java/com/yiming/InitalContext/InitialContext01.java
@@ -5,7 +5,7 @@ import com.yiming.service.impl.ServiceImpl2;
 
 //当缓存没有实体类对象时，执行初始化，加载接口实现类
 public class InitialContext01 {
-    
+
     public Object lookup(String jndiName){
         if(jndiName.equalsIgnoreCase("ServiceImpl1")){
             System.out.println("Looking up and creating a new ServiceImpl1 Object");
